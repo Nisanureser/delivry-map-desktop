c800dc86761b2c77aec40f3477e58cfc6cfb99fc
Nisanureser <Nisa2415@hotmail.com>
2026-01-21 20:49:31 +0300

    type dublicate  düzeltildi (coordnates)

--- types/delivery.types.ts
+++ types/delivery.types.ts
@@ -1,15 +1,14 @@
+import type { Coordinates } from './geocoding.types';
+
 export type Priority = 'high' | 'normal' | 'low';
-export interface Coordinates {
-    lat: number;
-    lng: number;
-  }
-  export interface DeliveryPoint {
-    id: string;
-    name: string;
-    address: string;
-    coordinates: Coordinates;
-    priority: Priority;
-    notes?: string;
-    order?: number;
-    placeId?: string;
-  }
+
+export interface DeliveryPoint {
+  id: string;
+  name: string;
+  address: string;
+  coordinates: Coordinates;
+  priority: Priority;
+  notes?: string;
+  order?: number;
+  placeId?: string;
+}
